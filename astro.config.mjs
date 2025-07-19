bb2d731fe95f1b18baa68b65d942b98a51807619
DominicDev <s0l0estudi4r@gmail.com>
2025-07-19 23:04:12 +0200

    v0.1.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,9 @@ export default defineConfig({
     assets: 'static'
   },
   //Deploy normal project
-  // base: '/project-000-948/',
-  // site: 'https://20essentials.github.io/project-000-948/'
-
-  //Deploy 20 Projects
-  // base: '/20-projects-with-nunjucks/',
-  // site: 'https://20essentials.github.io/20-projects-with-nunjucks/'
+  base: '/project-1011/',
+  site: 'https://20essentials.github.io/project-1011/'
 
   //Localhost
-  site: 'http://localhost:4321/'
+  // site: 'http://localhost:4321/'
 });
